e35eb09de9e788dd83b329bcd5acc8745593c53b
SenLief <senlief@163.com>
2016-06-16 13:14:17 +0800

    add doc

--- bookscatch.py
+++ bookscatch.py
@@ -1,10 +1,31 @@
 #!/usr/bin/env python
 #-*-coding=utf-8-*-
+'''
+============================================
+=======     Author : SenLief      ========== 
+=======     Version : 0.1         ==========
+============================================
+
+功能：小说抓取最新章节并保存到文件中
+原理：利用笔趣阁手机端抓取小说。
+		 手机端采用的是Cookies方式保存书籍，添加看到章节的书签到书架，利用抓取书架来比对最新章节
+		 和书签地址是否相同。
+
+Bug：
+	1、书签url不对会死循环。
+
+Furturs:
+	1、解决Bug
+	2、处理写到文件的格式问题
+	3、合并生成的文件到一个文件中
+	4、分量抓取
+'''
 
 import os
 import os.path
 import sys
 import re
+import time
 import requests
 import ConfigParser
 from bs4 import BeautifulSoup
@@ -14,19 +35,7 @@ from multiprocessing.dummy import Pool as ThreadPool
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-'''
-重写书籍抓取部分,利用笔趣阁书架的功能来完善书籍判断部分。主要抓取手机站，不再抓取电脑端。
 
-1、登录部分
-2、搜索书籍，返回书籍url地址（暂时不做）
-3、获取书籍目录并加入书架，添加最新章节为书签。
-4、实现书籍抓取，返回抓取内容
-5、流控：
-	1、遍历我的书架，判断最新章节和书签是否一致，一致则不抓取
-	2、不一致判断章节是否只有一章，一章则直接抓取
-	3、不是一章则和目录比对章节看有几章则抓取几章
-6、实现邮件推送服务
-'''
 headers = {
                  'User-Agent' : 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:47.0) Gecko/20100101 Firefox/47.0' ,
                  'Connection' : 'keep-alive',
@@ -36,7 +45,10 @@ headers = {
          }
 
 def get_case(cookies):
-	
+	'''
+		获取书籍书架，比对最新章节和书签url。
+		返回一个生成器的元祖。eg : （"/book/1/700033.html"，"/book/1/700023.html"）
+	'''	
 	resp = requests.get('http://m.biquge.la/shu.php',cookies=cookies,headers=headers)
 	resp.encoding = 'gbk'
 	
@@ -56,6 +68,11 @@ def get_case(cookies):
 		yield (caseList[0]['href'],caseList[0]['href'])
 
 def get_index(cid):
+	'''
+		获取书籍目录。
+		接受一个书籍cid
+		返回一个生成器的目录地址。 eg : "/book/1/700033.html"
+	'''
 
 	indexUrl = 'http://m.biquge.la/booklist/' + str(cid) +'.html'
 	indexResp = requests.get(indexUrl,headers=headers)
@@ -67,49 +84,56 @@ def get_index(cid):
 		yield url['href']
 
 def catch(url):
-
-	# url = 'http://m.biquge.la/book/511/7088514.html'
+	'''
+		抓取章节内容。
+		接受章节url参数，一般由下面的 index_num()函数生成
+		返回章节内容。
+	'''
+	
 	resp = requests.get(url,headers=headers)
 	resp.encoding = 'gbk'
 	
 	soup = BeautifulSoup(resp.text,'lxml')
 	title = soup.find(id='nr_title').text
 	content = soup.find(id='nr1').text
-	content = title + '\n' +  content.replace('shipei_x()',' ')
+	content = title +  content.replace('shipei_x()',' ')
 	return content
 
 def index_num(caseUrl):
-
-	# for cUrl, nUrl in get_case():
+	'''
+		获取需要抓取的地址，并抓取写到文件中。
+		接受参数为一个元祖变量，一般有get_case()函数返回.
+	'''
+	
 	bookId = caseUrl[0].split('/')[2]	
 	n = 0 
 	for url in get_index(bookId):	
 		if caseUrl[0] == caseUrl[1]:
 			catchUrl = 'http://m.biquge.la' + caseUrl[0]
-			# print catch(catchUrl)
 			with open(bookId + '.txt', 'a+') as f:
 				f.write(catch(catchUrl))
 			break
 		elif url == caseUrl[1]:
 			catchUrl = 'http://m.biquge.la' + url
-			# print catch(catchUrl)
 			with open(bookId + '.txt', 'a+') as f:
 				f.write(catch(catchUrl))
 			break
 		else:
 			catchUrl = 'http://m.biquge.la' + url
-			# print catch(catchUrl)
 			with open(bookId + '_' + str(n) + '.txt', 'a+') as f:
 				f.write(catch(catchUrl))
 			n = n + 1
 
 def main():
-	
+	'''
+		多线程并行入口。读取配置文件，多线程抓取书籍。
+	'''
 	cf = ConfigParser.ConfigParser()
 	cf.read("config.ini")
 	ck = {k:v for k,v in cf.items("cookies")}
 	ckid = {'cookie[{cid}]'.format(cid=cid) : newurl for cid, newurl in cf.items("books")}
 	ck.update(ckid)
+	
 	if os.path.exists("books"):
 		os.chdir("books")
 	else:
@@ -121,5 +145,5 @@ def main():
 	pool.close()
 	pool.join()
 
-
-main()
+if __name__ == '__main__':
+	main()
